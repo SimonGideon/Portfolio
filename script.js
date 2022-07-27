46eb6094b0326c68e5fed584ce77b318f39ad424
SimonGideon <simongideon918@gmail.com>
2022-07-27 17:49:04 +0300

    optimised: button-event-listener

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const hambuger = document.querySelector('.hambuger');
 
 const navMenu = document.querySelector('.nav-menu');
-
-// const navLinks = document.querySelectorAll(".nav-link");
-
 hambuger.addEventListener('click', () => {
   hambuger.classList.toggle('active');
   navMenu.classList.toggle('active');
   document.getElementById("bdy-bg").style.display = "none";
+  const btn1 = document.getElementById('wkbtn1');
+  const btn2 = document.getElementById('wkbtn2');
+  const btn3 = document.getElementById('wkbtn3');
+  const btn4 = document.getElementById('wkbtn4');
 });
 
 document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
