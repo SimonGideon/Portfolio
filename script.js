2423be14f63af0264d9eec026c5d572c82413543
SimonGideon <simongideon918@gmail.com>
2022-07-29 16:13:56 +0300

    initial commit

--- script.js
+++ script.js
@@ -317,5 +317,7 @@ function mailValidation(input) {
 contactForm.addEventListener('submit', (e) => {
   if (mailValidation(emailValidation.value) === false) {
     e.preventDefault();
-  } else {e.run()}
+  } else {
+    e.run()
+  }
 });
